9852215aa4c79f37ef4c69886b532542303325da
KikoFlorenti <sujitshankar1996@yahoo.co.in>
2021-02-11 16:21:54 +0530

    Creating DBs and basic Data Exploration with SELECT

---
+++ Exercises.sql
@@ -0,0 +1,58 @@
+-- CH01
+
+--1
+CREATE TABLE animal_types (
+   animal_type_id bigserial CONSTRAINT animal_types_key PRIMARY KEY,
+   common_name varchar(100) NOT NULL,
+   scientific_name varchar(100) NOT NULL,
+   conservation_status varchar(50) NOT NULL
+);
+
+--2a
+CREATE TABLE menagerie (
+   menagerie_id bigserial CONSTRAINT menagerie_key PRIMARY KEY,
+   animal_type_id bigint REFERENCES animal_types (animal_type_id),
+   date_acquired date NOT NULL,
+   gender varchar(1),
+   acquired_from varchar(100),
+   name varchar(100),
+   notes text
+);
+
+INSERT INTO animal_types (common_name, scientific_name, conservation_status)
+VALUES ('Bengal Tiger', 'Panthera tigris tigris', 'Endangered'),
+       ('Arctic Wolf', 'Canis lupus arctos', 'Least Concern');
+
+INSERT INTO menagerie (animal_type_id, date_acquired, gender, acquired_from, name, notes)
+VALUES
+(1, '3/12/1996', 'F', 'Dhaka Zoo', 'Ariel', 'Healthy coat at last exam.'),
+(2, '9/30/2000', 'F', 'National Zoo', 'Freddy', 'Strong appetite.');
+
+-- Testing how errors are dealt with
+--2b
+INSERT INTO animal_types (common_name, scientific_name, conservation_status)
+VALUES ('Javan Rhino', 'Rhinoceros sondaicus' 'Critically Endangered');
+
+
+
+
+--CH02
+
+--1
+SELECT school, first_name, last_name
+FROM teachers
+ORDER BY school, last_name;
+
+
+--2
+SELECT first_name, last_name, school, salary
+FROM teachers
+WHERE first_name LIKE 'S%' -- remember that LIKE is case-sensitive!
+      AND salary > 40000;
+
+--3
+SELECT last_name, first_name, school, hire_date, salary
+FROM teachers
+WHERE hire_date >= '2010-01-01'
+ORDER BY salary DESC;
+
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Practical-SQL
 
 PostgreSQL Tutorial and Practice
+
+Pracitcal SQL: A Beginner's Guide to Storytelling with Data by Anthony DeBarros
