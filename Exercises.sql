cf91b6756a5ad14c9455d27b933b90cc0a90df96
KikoFlorenti <sujitshankar1996@yahoo.co.in>
2021-02-14 18:34:44 +0530

    Exercises - CH06

--- Exercises.sql
+++ Exercises.sql
@@ -77,3 +77,49 @@ COPY (
      )
 TO 'C:\YourDirectory\us_counties_housing_export.txt'
 WITH (FORMAT CSV, HEADER);
+
+
+--CH06
+
+--1
+SELECT c2010.geo_name,
+       c2010.state_us_abbreviation,
+       c2000.geo_name
+FROM us_counties_2010 c2010 LEFT JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips
+WHERE c2000.geo_name IS NULL;
+
+
+
+
+SELECT c2010.geo_name,
+       c2000.geo_name,
+       c2000.state_us_abbreviation
+FROM us_counties_2010 c2010 RIGHT JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips
+WHERE c2010.geo_name IS NULL;
+
+
+--2
+
+SELECT median(round( (CAST(c2010.p0010001 AS numeric(8,1)) - c2000.p0010001)
+           / c2000.p0010001 * 100, 1 )) AS median_pct_change
+FROM us_counties_2010 c2010 INNER JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips;
+
+--3
+
+SELECT c2010.geo_name,
+       c2010.state_us_abbreviation,
+       c2010.p0010001 AS pop_2010,
+       c2000.p0010001 AS pop_2000,
+       c2010.p0010001 - c2000.p0010001 AS raw_change,
+       round( (CAST(c2010.p0010001 AS DECIMAL(8,1)) - c2000.p0010001)
+           / c2000.p0010001 * 100, 1 ) AS pct_change
+FROM us_counties_2010 c2010 INNER JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips
+ORDER BY pct_change ASC;
