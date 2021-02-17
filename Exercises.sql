ab7ec3e685405f87c98999e05e7ebd7e9b1ac2be
KikoFlorenti <sujitshankar1996@yahoo.co.in>
2021-02-17 12:18:26 +0530

    Completed Exercises for CH8,9

--- Exercises.sql
+++ Exercises.sql
@@ -147,4 +147,113 @@ CREATE TABLE songs (
     CONSTRAINT song_id_key PRIMARY KEY (song_id)
 );
 
+
+
+--CH08
+
+--1
+SELECT pls14.stabr,
+       sum(pls14.gpterms) AS gpterms_2014,
+       sum(pls09.gpterms) AS gpterms_2009,
+       round( (CAST(sum(pls14.gpterms) AS decimal(10,1)) - sum(pls09.gpterms)) /
+                    sum(pls09.gpterms) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.gpterms >= 0 AND pls09.gpterms >= 0
+GROUP BY pls14.stabr
+ORDER BY pct_change DESC;
+
+SELECT pls14.stabr,
+       sum(pls14.pitusr) AS pitusr_2014,
+       sum(pls09.pitusr) AS pitusr_2009,
+       round( (CAST(sum(pls14.pitusr) AS decimal(10,1)) - sum(pls09.pitusr)) /
+                    sum(pls09.pitusr) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.pitusr >= 0 AND pls09.pitusr >= 0
+GROUP BY pls14.stabr
+ORDER BY pct_change DESC;
+
 --2
+SELECT pls14.obereg,
+       sum(pls14.visits) AS visits_2014,
+       sum(pls09.visits) AS visits_2009,
+       round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
+                    sum(pls09.visits) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+GROUP BY pls14.obereg
+ORDER BY pct_change DESC;
+
+
+CREATE TABLE obereg_codes (
+    obereg varchar(2) CONSTRAINT obereg_key PRIMARY KEY,
+    region varchar(50)
+);
+
+INSERT INTO obereg_codes
+VALUES ('01', 'New England (CT ME MA NH RI VT)'),
+       ('02', 'Mid East (DE DC MD NJ NY PA)'),
+       ('03', 'Great Lakes (IL IN MI OH WI)'),
+       ('04', 'Plains (IA KS MN MO NE ND SD)'),
+       ('05', 'Southeast (AL AR FL GA KY LA MS NC SC TN VA WV)'),
+       ('06', 'Soutwest (AZ NM OK TX)'),
+       ('07', 'Rocky Mountains (CO ID MT UT WY)'),
+       ('08', 'Far West (AK CA HI NV OR WA)'),
+       ('09', 'Outlying Areas (AS GU MP PR VI)');
+
+
+SELECT obereg_codes.region,
+       sum(pls14.visits) AS visits_2014,
+       sum(pls09.visits) AS visits_2009,
+       round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
+                    sum(pls09.visits) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+   ON pls14.fscskey = pls09.fscskey
+JOIN obereg_codes
+   ON pls14.obereg = obereg_codes.obereg
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+GROUP BY obereg_codes.region
+ORDER BY pct_change DESC;
+
+
+
+SELECT pls14.libname, pls14.city, pls14.stabr, pls14.statstru, pls14.c_admin, pls14.branlib,
+       pls09.libname, pls09.city, pls09.stabr, pls09.statstru, pls09.c_admin, pls09.branlib
+FROM pls_fy2014_pupld14a pls14 FULL OUTER JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.fscskey IS NULL OR pls09.fscskey IS NULL;
+
+
+
+--CH09
+
+--1
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN meat_processing boolean;
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN poultry_processing boolean;
+
+SELECT * FROM meat_poultry_egg_inspect;
+
+
+UPDATE meat_poultry_egg_inspect
+SET meat_processing = TRUE
+WHERE activities ILIKE '%meat processing%'; -- case-insensitive match with wildcards
+
+UPDATE meat_poultry_egg_inspect
+SET poultry_processing = TRUE
+WHERE activities ILIKE '%poultry processing%'; -- case-insensitive match with wildcards
+
+
+SELECT * FROM meat_poultry_egg_inspect;
+
+
+SELECT count(meat_processing), count(poultry_processing)
+FROM meat_poultry_egg_inspect;
+
+
+
+SELECT count(*)
+FROM meat_poultry_egg_inspect
+WHERE meat_processing = TRUE AND
+      poultry_processing = TRUE;
